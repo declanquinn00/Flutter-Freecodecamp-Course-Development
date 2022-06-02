c82bc2cf2469cf5d052768846bb3ea4953ddb947
Declan Quinn <quinndeclan00@gmail.com>
2022-06-02 20:02:14 +0100

    Updated to return user to login verify or register screens

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/register_view.dart';
+import 'package:mynotes/views/verify_email_view.dart';
 
 import 'firebase_options.dart';
 
@@ -38,6 +39,19 @@ class Homepage extends StatelessWidget {
         builder:(context, snapshot) { // snapshot is a state returns the result of our future
           switch (snapshot.connectionState){ // What do we do while connecting
             case ConnectionState.done: // If finished user will see this
+              final user = FirebaseAuth.instance.currentUser;
+              if (user != null){
+                if (user.emailVerified){
+                  print('Email is verified');
+                }
+                else{
+                  return const VerifyEmailView();
+                }
+              }
+              else {
+                return const LoginView();
+              }
+              return const Text('Done');
                     //final user = FirebaseAuth.instance.currentUser; // ?? -> if user.emailVerified exists take that else false
                     //final emailVerified = user?.emailVerified ?? false;
                     //if(emailVerified == true){
