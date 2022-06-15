ebb8b707a6b3bc9bc16c915e3037b66e1e0f4cf8
Declan Quinn <quinndeclan00@gmail.com>
2022-06-15 20:16:49 +0100

    Created unit tests for auth service

--- lib/services/auth/auth_user.dart
+++ lib/services/auth/auth_user.dart
@@ -4,8 +4,8 @@ import 'package:flutter/cupertino.dart'; // Only show user class
 @immutable  // Internals of this class will never be changed once initialized subclasses cannot have fields that change
 class AuthUser{
   final bool isEmailVerified;
-  const AuthUser(this.isEmailVerified);
+  const AuthUser({required this.isEmailVerified}); // Curly braces and required so we can check if isEmailVerified is true in testing
 
-  factory AuthUser.fromFirebase(User user) => AuthUser(user.emailVerified); // takes emailverified from firebase user and places in Authuser
+  factory AuthUser.fromFirebase(User user) => AuthUser(isEmailVerified: user.emailVerified); // takes emailverified from firebase user and places in Authuser
 }
 
