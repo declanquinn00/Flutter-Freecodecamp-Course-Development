2d81171003efdeb9b407bd78277bce1461dc970c
Declan Quinn <quinndeclan00@gmail.com>
2022-06-23 21:59:53 +0100

    created new notes view, added icon button to notes view and fixed unit tests

--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:mynotes/services/auth/auth_service.dart';
 import 'dart:developer' as devtools show log; 
-import '../constants/routes.dart';
-import '../enums/menu_action.dart';
-import '../services/crud/notes_service.dart';
+import '../../constants/routes.dart';
+import '../../enums/menu_action.dart';
+import '../../services/crud/notes_service.dart';
 
 class NotesView extends StatefulWidget {
   const NotesView({ Key? key }) : super(key: key);
@@ -35,8 +35,14 @@ class _NotesViewState extends State<NotesView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Main UI'),
+        title: const Text('My Notes'),
         actions: [
+        IconButton( // Adds a plus Button to go to new note view
+          onPressed: (){
+            Navigator.of(context).pushNamed(newNoteRoute); // Allows us to go back easily to notes view
+          }, 
+          icon: const Icon(Icons.add)
+        ),
          PopupMenuButton<MenuAction>(
             onSelected: (value) async {
               switch (value){
