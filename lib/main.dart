82d77e3e938d00095ce0d88ab37a19e913e79ab0
Declan Quinn <quinndeclan00@gmail.com>
2022-08-01 22:56:53 +0100

    removed debug banner from the screen

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ void main() {
   runApp(
     MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
